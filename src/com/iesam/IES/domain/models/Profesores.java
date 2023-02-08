fd699a02e4be21d03e4fe57476cc8b44023bd102
JuanGarArr <juan.gararr@educa.jcyl.es>
2023-02-08 13:01:25 +0100

    Creaccion de estuctura carpetas, imagen en assetsm y diferentes clases

---
+++ src/com/iesam/IES/domain/models/Profesores.java
@@ -0,0 +1,23 @@
+package com.iesam.IES.domain.models;
+
+public class Profesores extends Persona {
+
+    private String tituloAcademico;
+    private String fechaAlta;
+
+    public String getTituloAcademico() {
+        return tituloAcademico;
+    }
+
+    public void setTituloAcademico(String tituloAcademico) {
+        this.tituloAcademico = tituloAcademico;
+    }
+
+    public String getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(String fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+}
